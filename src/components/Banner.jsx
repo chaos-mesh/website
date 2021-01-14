a7c90ea254f89b55ee741618b346efcac6bd90d0
Yue Yang <g1enyy0ung@gmail.com>
2021-01-14 17:31:25 +0800

    chore: update
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 const Banner = () => {
   const content =
-    '🙋‍♀️ 🙋‍♂️ Contributors & adopters, take the community&nbsp;<a href="https://bit.ly/3i3v2Vr" target="_blank">survey</a>&nbsp;and claim your Chaos Mesh anniversary swag!'
+    '🙋‍♀️ 🙋‍♂️ Contributors & adopters, take the&nbsp;<a href="https://bit.ly/3i3v2Vr" target="_blank">community survey</a>&nbsp;and claim your Chaos Mesh anniversary swag!'
 
   useEffect(() => {
     if (document.body.contains(document.querySelector('#banner'))) {
