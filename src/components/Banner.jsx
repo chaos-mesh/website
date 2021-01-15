9850e2e592fd204702a1aae5c68d878359b654bd
Yue Yang <g1enyy0ung@gmail.com>
2021-01-15 10:14:03 +0800

    chore: now can close banner
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,13 +5,24 @@ const Banner = () => {
     '🙋‍♀️ 🙋‍♂️ Contributors & adopters, take the&nbsp;<a href="https://bit.ly/3i3v2Vr" target="_blank">community survey</a>&nbsp;and claim your Chaos Mesh anniversary swag!'
 
   useEffect(() => {
-    if (document.body.contains(document.querySelector('#banner'))) {
+    if (document.body.contains(document.querySelector('#banner')) || window.closeBanner) {
       return
     }
 
     const d = document.createElement('div')
     d.id = 'banner'
     d.innerHTML = content
+
+    const close = document.createElement('span')
+    close.className = 'close'
+    close.innerHTML = 'x'
+    close.onclick = () => {
+      d.remove()
+
+      window.closeBanner = true
+    }
+
+    d.append(close)
 
     document.querySelector('#__docusaurus').prepend(d)
   }, [])
