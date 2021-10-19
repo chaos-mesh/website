88f0a90290bd9606ae2881c77c0ed949c54f4e68
Yue Yang <g1enyy0ung@gmail.com>
2021-10-19 17:56:41 +0800

    docs: v2.0.3
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- src/components/PickHelmVersion.jsx
+++ src/components/PickHelmVersion.jsx
@@ -1,22 +1,23 @@
 import BrowserOnly from '@docusaurus/BrowserOnly'
 import CodeBlock from '@theme/CodeBlock'
 import React from 'react'
+import semver from 'semver'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import { usePickVersion } from './PickVersion'
 import { usePluginData } from '@docusaurus/useGlobalData'
 
 const calcHelmChartVersion = (version) => {
-  if (version[0] === '2') {
+  if (semver.satisfies(version, '>=2.0.3')) {
     return version
   }
 
   const startPart = version.slice(0, 3)
   const helmChartVersionStartPart = parseFloat(startPart) - 0.7
 
-  return helmChartVersionStartPart + version.slice(3)
+  return 'v' + helmChartVersionStartPart + version.slice(3)
 }
 
-const PickHelmVersion = ({ children, className }) => {
+const PickHelmVersion = ({ children, className = 'language-bash' }) => {
   const { siteConfig } = useDocusaurusContext()
   const { versions } = usePluginData('docusaurus-plugin-content-docs')
 
@@ -24,7 +25,7 @@ const PickHelmVersion = ({ children, className }) => {
     <BrowserOnly>
       {() => {
         const version = usePickVersion(siteConfig, versions)
-        const realVersion = version === 'latest' ? '' : `--version v${calcHelmChartVersion(version)}`
+        const realVersion = version === 'latest' ? '' : `--version ${calcHelmChartVersion(version)}`
 
         return <CodeBlock className={className}>{children.replace('--version latest', realVersion).trim()}</CodeBlock>
       }}
