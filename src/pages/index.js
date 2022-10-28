d41ed7eddc63855fa9f483d32b2fd9d442cb57fb
Yue Yang <g1enyy0ung@gmail.com>
2022-10-28 18:17:11 +0800

    chore: update
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,17 +17,31 @@ import styles from './index.module.css'
 const description =
   'Chaos Mesh brings various types of fault simulation to Kubernetes and has an enormous capability to orchestrate fault scenarios. It helps you conveniently simulate various abnormalities that might occur in reality during the development, testing, and production environments and find potential problems in the system.'
 
-function Feature({ imgUrl, title, description, reverse }) {
+function Feature({ imgUrl, title, description, className }) {
+  const isKubernetes = imgUrl === 'img/logos/kubernetes.svg'
+
   return (
-    <div className={clsx('row', 'tw-mb-6 last:tw-mb-0 md:tw-mb-16', reverse && 'tw-flex-row-reverse')}>
-      <div className="col col--6 tw-text-center">
-        <img className="tw-h-48 tw-mb-6 lg:tw-h-64 lg:tw-mb-0" src={useBaseUrl(imgUrl)} alt={title} />
-      </div>
-      <div className="col col--6 tw-flex tw-items-center">
-        <div>
-          <h3>{title}</h3>
-          <div>{description}</div>
+    <div
+      className={clsx(
+        'tw-p-6 tw-border-solid tw-border-1 tw-border-base-content tw-border-opacity-10 tw-rounded-2xl',
+        className
+      )}
+    >
+      <div className={clsx('tw-relative tw-flex tw-items-center tw-h-full', isKubernetes && 'tw-flex-col')}>
+        {!isKubernetes && (
+          <div className="tw-flex-1 tw-text-center">
+            <img className="tw-w-[80%]" src={useBaseUrl(imgUrl)} alt={title} />
+          </div>
+        )}
+        <div className="tw-flex-1">
+          <div>
+            <h3>{title}</h3>
+            <div>{description}</div>
+          </div>
         </div>
+        {isKubernetes && (
+          <img className="tw-absolute tw-right-6 -tw-bottom-12 tw-h-48" src={useBaseUrl(imgUrl)} alt={title} />
+        )}
       </div>
     </div>
   )
@@ -49,7 +63,7 @@ function Home() {
         <div className="hero tw-relative tw-h-[768px] tw-pt-0 tw-overflow-hidden">
           <BrowserOnly>{() => <Mesh />}</BrowserOnly>
           <div className="tw-container tw-mx-auto tw-z-10">
-            <div className="tw-flex tw-flex-col lg:tw-flex-row md:tw-justify-around lg:tw-items-center">
+            <div className="tw-flex tw-flex-col lg:tw-flex-row md:tw-justify-between lg:tw-items-center">
               <div className="tw-flex-[.8] 2xl:tw-flex-[.6] tw-p-6 lg:tw-p-3">
                 <h1
                   className={clsx(
@@ -103,7 +117,12 @@ function Home() {
               <div className="row max-md:tw-w-[1280px]">
                 {whoIsUsing.map((w) => (
                   <div key={w.name} className={clsx('col col--1', styles.whiteboardCol)}>
-                    <a className="tw-flex tw-justify-center tw-items-center tw-h-[100px]" href={w.href} target="_blank">
+                    <a
+                      className="tw-flex tw-justify-center tw-items-center tw-h-[100px]"
+                      href={w.href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <img style={w.style} src={useBaseUrl(w.img)} alt={w.name} />
                     </a>
                   </div>
@@ -115,112 +134,121 @@ function Home() {
 
         <div className="hero">
           <div className="tw-container tw-mx-auto tw-px-4">
-            <Feature
-              imgUrl="img/features/undraw_server_down_s4lk.svg"
-              title={<Translate id="home.easytouse">Easy to Use</Translate>}
-              description={
-                <>
-                  <p>
-                    <Translate
-                      id="home.easytouse.1"
-                      values={{
-                        minikube: <Link to="https://minikube.sigs.k8s.io/">minikube</Link>,
-                        kind: <Link to="https://kind.sigs.k8s.io/">kind</Link>,
-                      }}
-                    >
-                      {
-                        'No special dependencies, Chaos Mesh can be easily deployed on Kubernetes clusters directly, including {minikube} and {kind}.'
-                      }
-                    </Translate>
-                  </p>
-                  <ul>
-                    <li>
-                      <Translate id="home.easytouse.2">
-                        Ability to perform chaos experiments in production environments without modifying the deployment
-                        logic of the application.
+            <h2 className="tw-text-3xl xl:tw-text-4xl tw-max-w-[600px] xl:tw-max-w-[700px] tw-mb-12">
+              Make <span className={styles.heroTitle}>Cloud Native + Chaos Engineering</span> simple and
+              straightforward.
+            </h2>
+
+            <div className="tw-grid tw-gap-8 tw-grid-rows-2 tw-grid-cols-6">
+              <Feature
+                imgUrl="img/logos/kubernetes.svg"
+                title={<Translate id="home.k8s">Design for Kubernetes</Translate>}
+                description={
+                  <>
+                    <p>
+                      <Translate
+                        id="home.k8s.1"
+                        values={{
+                          crd: (
+                            <Link to="https://kubernetes.io/docs/concepts/extend-kubernetes/api-extension/custom-resources/">
+                              CustomResourceDefinition (CRD)
+                            </Link>
+                          ),
+                        }}
+                      >
+                        {'Chaos Mesh uses {crd} to define chaos experiments.'}
                       </Translate>
-                    </li>
-                    <li>
-                      <Translate id="home.easytouse.3">
-                        Easily orchestrate the behavior of chaos experiments, allowing users to observe the state of the
-                        experiment itself in real time and quickly rollback any injected failures.
+                    </p>
+                    <p>
+                      <Translate id="home.k8s.2">
+                        In the Kubernetes realm, CRD is a proven solution for implementing custom resources. CRD enables
+                        the natural integration of Chaos Mesh with the Kubernetes ecosystem.
                       </Translate>
-                    </li>
-                    <li>
-                      <Translate id="home.easytouse.4">
-                        Packed with dashboard. No handwritten experiment definitions are required, and a chaos
-                        experiment can be run smoothly in just a few clicks.
+                    </p>
+                  </>
+                }
+                className="tw-col-span-2"
+              />
+              <Feature
+                imgUrl="img/features/undraw_server_down_s4lk.svg"
+                title={<Translate id="home.easytouse">Easy to Use</Translate>}
+                description={
+                  <>
+                    <p>
+                      <Translate
+                        id="home.easytouse.1"
+                        values={{
+                          minikube: <Link to="https://minikube.sigs.k8s.io/">minikube</Link>,
+                          kind: <Link to="https://kind.sigs.k8s.io/">kind</Link>,
+                        }}
+                      >
+                        {
+                          'No special dependencies, Chaos Mesh can be easily deployed on Kubernetes clusters directly, including {minikube} and {kind}.'
+                        }
                       </Translate>
-                    </li>
-                  </ul>
-                </>
-              }
-            />
-            <Feature
-              imgUrl="img/logos/kubernetes.svg"
-              title={<Translate id="home.k8s">Design for Kubernetes</Translate>}
-              description={
-                <>
+                    </p>
+                    <ul>
+                      <li>
+                        <Translate id="home.easytouse.2">
+                          Ability to perform chaos experiments in production environments without modifying the
+                          deployment logic of the application.
+                        </Translate>
+                      </li>
+                      <li>
+                        <Translate id="home.easytouse.3">
+                          Easily orchestrate the behavior of chaos experiments, allowing users to observe the state of
+                          the experiment itself in real time and quickly rollback any injected failures.
+                        </Translate>
+                      </li>
+                      <li>
+                        <Translate id="home.easytouse.4">
+                          Packed with dashboard. No handwritten experiment definitions are required, and a chaos
+                          experiment can be run smoothly in just a few clicks.
+                        </Translate>
+                      </li>
+                    </ul>
+                  </>
+                }
+                className="tw-col-span-4"
+              />
+              <Feature
+                imgUrl="img/features/undraw_stars_re_6je7.svg"
+                title={<Translate id="home.failuretypes">A wide variety of failure types</Translate>}
+                description={
                   <p>
-                    <Translate
-                      id="home.k8s.1"
-                      values={{
-                        crd: (
-                          <Link to="https://kubernetes.io/docs/concepts/extend-kubernetes/api-extension/custom-resources/">
-                            CustomResourceDefinition (CRD)
-                          </Link>
-                        ),
-                      }}
-                    >
-                      {'Chaos Mesh uses {crd} to define chaos experiments.'}
+                    <Translate id="home.failuretypes.1">
+                      Chaos Mesh initially started from a distributed system perspective, fully considering the possible
+                      failures of distributed systems, thus providing a more comprehensive and fine-grained fault type
+                      to help users with fault injection for network, disk, file system, operating system, etc. in a
+                      comprehensive manner.
                     </Translate>
                   </p>
-                  <p>
-                    <Translate id="home.k8s.2">
-                      In the Kubernetes realm, CRD is a proven solution for implementing custom resources. CRD enables
-                      the natural integration of Chaos Mesh with the Kubernetes ecosystem.
-                    </Translate>
-                  </p>
-                </>
-              }
-              reverse={true}
-            />
-            <Feature
-              imgUrl="img/features/undraw_stars_re_6je7.svg"
-              title={<Translate id="home.failuretypes">A wide variety of failure types</Translate>}
-              description={
-                <p>
-                  <Translate id="home.failuretypes.1">
-                    Chaos Mesh initially started from a distributed system perspective, fully considering the possible
-                    failures of distributed systems, thus providing a more comprehensive and fine-grained fault type to
-                    help users with fault injection for network, disk, file system, operating system, etc. in a
-                    comprehensive manner.
-                  </Translate>
-                </p>
-              }
-            />
-            <Feature
-              imgUrl="img/features/undraw_safe_re_kiil.svg"
-              title={<Translate id="home.safe">Safe and Controllable</Translate>}
-              description={
-                <>
-                  <p>
-                    <Translate id="home.safe.1">
-                      Chaos Mesh provides role-based access control. Users can create roles with corresponding
-                      permissions according to their needs, such as visitor roles, administrative roles, etc.
-                    </Translate>
-                  </p>
-                  <p>
-                    <Translate id="home.safe.2">
-                      In addition, Chaos Mesh supports setting up Namespace whitelists and blacklists, which allow users
-                      to protect important Namespaces and thus gain greater control over the "blast radius" of
-                      experiments.
-                    </Translate>
-                  </p>
-                </>
-              }
-              reverse={true}
-            />
+                }
+                className="tw-col-span-3"
+              />
+              <Feature
+                imgUrl="img/features/undraw_safe_re_kiil.svg"
+                title={<Translate id="home.safe">Safe and Controllable</Translate>}
+                description={
+                  <>
+                    <p>
+                      <Translate id="home.safe.1">
+                        Chaos Mesh provides role-based access control. Users can create roles with corresponding
+                        permissions according to their needs, such as visitor roles, administrative roles, etc.
+                      </Translate>
+                    </p>
+                    <p>
+                      <Translate id="home.safe.2">
+                        In addition, Chaos Mesh supports setting up Namespace whitelists and blacklists, which allow
+                        users to protect important Namespaces and thus gain greater control over the "blast radius" of
+                        experiments.
+                      </Translate>
+                    </p>
+                  </>
+                }
+                className="tw-col-span-3"
+              />
+            </div>
           </div>
         </div>
 
